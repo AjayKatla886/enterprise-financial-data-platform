b65406614031c541bcc682558ed6690de11198f7
Ajay Katla <ajay.katla@fmr.com>
2026-09-05 18:33:13 -0400

    feat: initialize customer service

--- README.md
+++
@@ -1,2 +0,0 @@
-# enterprise-financial-data-platform
-Production ready java full stack financial platform build with spring boot, angular, microservices, kafka, spring batch, oracle,aws
---
+++ backend/customer-service/src/test/java/com/financialplatform/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialplatform.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
